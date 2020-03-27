ca804a3009ad63b8de0bdf6a44fe4faff3e21536
ChangJoEconomy <47021837+ChangJoEconomy@users.noreply.github.com>
2020-03-27 22:43:50 +0900

    Update chart-pie-demo.js

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["negative", "under examination", "Positive"],
     datasets: [{
-      data: [92.5, 4.8, 2.7],
+      data: [93.5, 4.0, 2.5],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
